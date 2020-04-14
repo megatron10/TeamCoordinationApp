aea75cedfa08f40c2b593c9099c0e1ac33f5423c
dhananjay <cs17btech11014@iith.ac.in>
2020-04-14 23:17:10 +0530

    another init

--- main.py
+++ main.py
@@ -4,13 +4,43 @@ import multiprocessing
 
 def process1():
     from task1 import main
+
     print("ID of process running process1: {}".format(os.getpid()))
     main()
 
 
 def process2():
     from task2 import main
+
     print("ID of process running process2: {}".format(os.getpid()))
+    main()
+
+
+def process3():
+    from task3 import main
+
+    print("ID of process running process3: {}".format(os.getpid()))
+    main()
+
+
+def process4():
+    from task4 import main
+
+    print("ID of process running process4: {}".format(os.getpid()))
+    main()
+
+
+def process5():
+    from task5 import main
+
+    print("ID of process running process5: {}".format(os.getpid()))
+    main()
+
+
+def process6():
+    from task6 import main
+
+    print("ID of process running process6: {}".format(os.getpid()))
     main()
 
 
@@ -21,13 +51,25 @@ if __name__ == "__main__":
     # creating processes
     p1 = multiprocessing.Process(target=process1)
     p2 = multiprocessing.Process(target=process2)
+    p3 = multiprocessing.Process(target=process3)
+    p4 = multiprocessing.Process(target=process4)
+    p5 = multiprocessing.Process(target=process5)
+    p6 = multiprocessing.Process(target=process6)
 
     # starting processes
     p1.start()
     p2.start()
+    p3.start()
+    p4.start()
+    p5.start()
+    p6.start()
 
     # wait until processes are finished
     p1.join()
     p2.join()
+    p3.join()
+    p4.join()
+    p5.join()
+    p6.join()
 
     print("finished main process")
--- task2.py
+++ task2.py
@@ -1,7 +1,17 @@
-import time
+import asyncio
+import json
+import websockets
+
+
+async def communicate(websocket, path):
+    data = await websocket.recv()
+    data = json.loads(data)
+    tosend = {"a": "b"}
+    tosend = json.dumps(tosend)
+    await websocket.send(tosend)
 
 
 def main():
-    print("starting task2")
-    time.sleep(11)
-    print("done task2")
+    start_server = websockets.serve(communicate, "localhost", 8765)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
