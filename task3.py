ce6091841df28e961fa78369366c8437ea14150c
megatron10 <cs17btech11035@iith.ac.in>
2020-04-17 00:23:26 +0530

    send messages updated

--- task3.py
+++ task3.py
@@ -5,9 +5,8 @@ import asyncio
 import websockets
 from utils import check_valid_sid
 
-USERS = set()
+USER_to_UID = {}
 UID_to_USER = {}
-val = 0
 
 
 def get_msg(data):
@@ -21,17 +20,7 @@ async def send_message_to_channel(data):
     message = get_msg(data)
     sender = data['uid']
 
-    if data['action'] == 'connect':
-        await UID_to_USER[sender].send(message)
-        return 0
-
-    for uid in ls:
-        if uid in UID_to_USER and UID_to_USER[uid] not in USERS:
-            UID_to_USER.pop(uid, 'uid not found')
-
-    print(data['message'])
-
-    to = [uid for uid in ls if uid != sender and UID_to_USER[uid] in USERS]
+    to = [uid for uid in ls if uid != sender and uid in UID_to_USER]
     if len(to) > 0:  # asyncio.wait doesn't accept an empty. use list len(to) > 0 and
         await asyncio.wait([UID_to_USER[uid].send(message) for uid in to])
     return 0
@@ -42,47 +31,51 @@ async def is_valid_send_request(uid, channel):
     # return await db query, is user uid allowed to use channel
     return True
 
-
-# this is executed when the user first connects its web socket
-def register(websocket):
-    USERS.add(websocket)
-
+# this is executed when the user first connects and sends a 'connect' action message
+async def register(websocket, uid, message):
+    USER_to_UID[websocket] = uid
+    UID_to_USER[uid] = websocket
+    await websocket.send(message)
 
 def unregister(websocket):
-    USERS.remove(websocket)
-
+    if websocket in USER_to_UID:
+        user = USER_to_UID[websocket]
+        UID_to_USER.pop(user, 'user not found')
+        USER_to_UID.pop(websocket)
 
 async def communicate(websocket, path):
     '''
-    To the client, just populate uid, sid, channel, action and message in each
-    request. The first message must have action field set to connect. All fields
-    are at all times mandatory. Tho the values in these fields may not matter.
-    For example in the connect action (i.e. first interaction)
-    On successfully connecting the client will get a response message.
-    '''
-    register(websocket)
+    To the client, just populate uid, sid, channel, action and message fields in each request.
+    For first message use action = 'connect'
+    To send messages use action = 'send'
+    You will be notified of incoming messages from this websocket after you have connected.
+    '''	
 
     try:
         async for message in websocket:
             data = json.loads(message)
-            # first message
+            
+            # invalid session id
             if not check_valid_sid(data['uid'], data['sid']):
                 print('problem')
                 await websocket.close()
                 break
+            
+            # connect and get confirmation message echoed back. Can send messages after this and will receive messages for other users messages.
+            elif data['action'] == 'connect':
+                await register(websocket, data['uid'], message)
+
+            # send a message
+            elif data['action'] == 'send':
+            	if await is_valid_send_request(data['uid'], data['channel']):
+                	await send_message_to_channel(data)
+            	else:
+                	await websocket.send(json.dumps({"message": "invalid request, can't send message on this channel"}))
+            
+            # not a recognized action
             else:
-                UID_to_USER[data['uid']] = websocket
-
-            is_cool = True
-            if data['action'] != 'connect':  # It can be 'send'
-                is_cool = await is_valid_send_request(data['uid'], data['channel'])
-
-            # print(is_cool)
-            if is_cool:
-                await send_message_to_channel(data)
-            else:
-                await websocket.send(json.dumps({"message": "invalid request"}))
-
+            	await websocket.send(json.dumps({"message": "invalid action"}))
+    
     finally:
         unregister(websocket)
 
