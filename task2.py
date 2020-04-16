137d1d52e85593febb466b8e9cad31e3a5417341
megatron10 <cs17btech11035@iith.ac.in>
2020-04-16 23:32:13 +0530

    online status done! this process keeps track of online status of all users and informs the db and other users

--- task2.py
+++ task2.py
@@ -1,25 +1,81 @@
+# import time
+# import random
 import json
 import asyncio
 import websockets
 from utils import check_valid_sid
 
+USER_to_UID = {}
+UID_to_USER = {}
+
+
+def inform_status_msg(uid, status):
+    return json.dumps({'from_uid': uid, "message": status})
+
+async def send_online_contacts_list(websocket, uid):
+    # await query db for online contacts of 'uid'
+    ls = ['sahil', 'dhanno', 'anjani']
+    await websocket.send(json.dumps({"action": 'list', "message": repr(ls)}))
+
+async def inform_contacts(uid, status):
+    #await query db for online contacts of 'uid'
+    ls = ['sahil', 'dhanno', 'anjani']
+    to = [contact_id for contact_id in ls if contact_id in UID_to_USER]
+    if len(to) > 0:  # asyncio.wait doesn't accept an empty. use list len(to) > 0 and
+        await asyncio.wait([UID_to_USER[contact_id].send(inform_status_msg(uid, status)) for contact_id in to])
+    return 0
+
+# this is executed when the user first connects its web socket
+async def register(websocket, data):
+    UID_to_USER[data['uid']] = websocket
+    USER_to_UID[websocket] = data['uid']
+    # await update online status in DB for user 'uid'
+    await send_online_contacts_list(websocket, data['uid'])
+    await inform_contacts(data['uid'], 'online')
+
+async def unregister(websocket):
+    if websocket in USER_to_UID:
+        user = USER_to_UID[websocket]
+        UID_to_USER.pop(user, 'user not found')
+        USER_to_UID.pop(websocket)
+        # await update status of 'user' to offline in db
+        await inform_contacts(user, 'offline')
+    return 0
 
 async def communicate(websocket, path):
-    # await register(websocket) . update local variables new connection established.
-
+    '''
+    To the client, just populate uid, sid, action and request in each
+    request. The first message must have action field set to connect. All fields
+    are at all times mandatory.
+    On connecting the server will send the client a list of online users in the org.
+    The server shall send messages to inform about any change in online status of users.
+    '''
+    
     try:
         async for message in websocket:
             data = json.loads(message)
-            tosend = {"a": "b"}
-            tosend = json.dumps(tosend)
-            await websocket.send(tosend)
-
+            
+            # invalid session id
+            if not check_valid_sid(data['uid'], data['sid']):
+                print('problem')
+                await websocket.close()
+                break
+            # connect and get list of online contacts, after this client will be updated whenever a contact changes status
+            elif data['action'] == 'connect':
+                await register(websocket, data)
+            # invalid request, this process does nothing else
+            else:
+                await websocket.send(json.dumps({"message": "invalid request"}))
+    
     finally:
-        pass
-        # await unregister(websocket). update local variables, connection broken.
+        await unregister(websocket)
 
 
-def main(port=9002):
+def main(port=9003):
     start_server = websockets.serve(communicate, "localhost", port)
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
+
+
+if __name__ == '__main__':
+    main()
