32281dbecb391edd34066367101d8da4289ab195
vijayphoenix <cs17btech11040@iith.ac.in>
2020-05-13 20:24:25 +0530

    Use Relative paths

--- login.js
+++ login.js
@@ -4,12 +4,40 @@ loginBtn.addEventListener("click", function() {
     var password = document.getElementById("pswrd").value;
     check(username, password);
 });
- 
+
+function absFileLoc(filename) {
+    var scriptElements = document.getElementsByTagName('script');
+    for (var i = 0; i < scriptElements.length; i++) {    
+        var source = scriptElements[i].src;
+        if (source.indexOf(filename) > -1) {
+            var location = source.substring(0, source.indexOf(filename)) + filename;
+            return location;
+        }
+    }
+    return false;
+}
+
+function absolute(base, relative) {
+    var stack = base.split("/"),
+        parts = relative.split("/");
+    stack.pop(); // remove current file name (or empty string)
+    for (var i=0; i<parts.length; i++) {
+        if (parts[i] == ".")
+            continue;
+        if (parts[i] == "..")
+            stack.pop();
+        else
+            stack.push(parts[i]);
+    }
+    return stack.join("/");
+}
+
 function check(username, password)
 {
     if (username == 'bill' && password == 'cosby')
     {
-        location.replace(`file:///home/deeptanshu/Templates/CN_project/networks-electron/html/chat.html`);
+        loc = absolute(absFileLoc("login.js"), "html/chat.html")
+        location.replace(loc);
     }
     else
     {
